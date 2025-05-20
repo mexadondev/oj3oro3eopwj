2e560c1f9622fb145a130e2645bafc18bdcc8a6d
mexadondev <dimaakk131255@gmail.com>
2025-05-20 15:56:32 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,80 @@
+#ТОКЕНЫ
+token = '7587996693:AAFSDOLf6G0vpoEXEv_FO4fKiwLGgfW1iVI' #Токен бота
+api_cryptobot = '385507:AAUgEwtlUl12rc80RRdZtOEX4VMd9X2EExh' # api CryptoBot
+
+#id каналов/чатов
+channal_id = -1002503157194 #id игрового канала (Где происходят игры)
+ID_SEND_TRANSFER = -1002533025006 #Канал выплат
+URL_LOG_CHANNAL = -1002553969946 #id канала логов
+
+
+#ОБЩЕЕ
+ADMIN = [7304732856, 586766546] #id Админов, перечислять через запятую
+ADMIN_USERNAME = '@pokodil, @ccuzs'  #@Юзерней админа
+URL_BOT = 'https://t.me/ckammircasinobot' #Ссылка на бота
+NICNAME = 'ckammircasinobot' #Username бота без @
+NAME_CASINO = 'Ckammir Casino' #Название вашего казино
+
+
+#ЛИМИТЫ И ПРОЦЕНТЫ
+LIMIT_STAVKA = 30 #Лимит на max ставку
+lose_withdraw = 20 #Процент реферальной программы
+min_stavka_referal = 10 #Это минимальная сумма ставки для того чтобы получить деньги с реферала
+CASHBACK_PROCENT = 10 #Процент кэшбэка от ставки при проигрыше
+CASHBACK_LIMIT = 1 #От какой суммы ставки предоставляется кэшбэк
+PROCENT_DRAW = 7 #Процент комиссии за ничью
+PROCENT_NOT_GAME = 10 #Процент за комиссию при привышении макс ставки или не указания коментария к платежу
+TIMER = 46 #Указываем время в секундах в какое n время будет играть фейк ставка когда она будет включена через админ меню, рекомендуемое значение 40 секунд
+
+
+
+#Фейк ставки
+FAKE_NICKNAME = ['Strategovv', 'Alimov', 'methadonova', 'Луd1к', 'Madonna', 
+    'Vortie', 'Hexov', 'Панфилов', 'Паровозов', 'Максimov [Check Bio]', 
+    'Evgeniy Kostelomov', 'Карим', 'Аlyev', 'NullbyteX', 'Gh0st_404', 'Overcl0cked',
+    'sys32burner', 'DarkP1xel', 'XploitVibe', 'Crypt0wraith', 'Z3r0Trace',
+    'packet_sn1ffer', 'BitcrashX', 'HexeD00m', '0verfl0w.exe', 'Night_M0d3',
+    'CTRL_ALT_DELTA', '404Handler', '0xPhantom', 'Cyb3rSt4tic', 'RebootXx',
+    'KernelPanic', 'MetaMorph_1337', 'xX_PingLord_Xx', 'TrojanOwl', '0xDEADCAFE',
+    'ProxyPunk', 'loop.exe', 'ByteRipper', 'Wiresh4rkz', 'ShadowDump', 'N3tw0rkGhoul', 
+    'gl1tch_RUS', '0xBADC0DE', 'D3C0D3R', 'SudoK1ll', 'bytebl33d', 
+    'LagSw1tch', 'KompromatX', 'ctrl_zombie', 'Malw4rsky', 'Z3r0Kasp3r',
+    'darknet_vlad', 'QbitQuake', 'Rootovik', 'SilentBoot', 'Hacknobyl',
+    'V!rusVoid', 'GhostShellRU', 'DDoSya', 'SysCrash91', 'R3bootnik',
+    'NoSignalX', '1nject0r', '0ut0fR4nge', 'EXE_Lich', 'NullVodka',
+    'Overclockov', 'Shadovnik', '4rch0n_Bl33d', 'CMD_Olezhka', 'TermiNull',
+    'BitBand1t', 'SSHniper', 'Anonimkin', 'RAMraid3r', 'Zam3tka.exe'] #Фейк ники
+DIAPAZONE_AMOUNT = [0.50, 2.47] #Диапозон сумм фейк ставки
+
+
+#////////////////////////////////Ключевые слова. НЕ ТРОГАТЬ\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+#Кости
+Bones = ['Больше', 'Меньше', '1', '2', '3', '4', '5', '6', 'Пвп', 'Ничья', 'Равно', 'Дуэль', '2М', '2Б', '2 Больше', '2 Меньше', 'Чет', 'Нечет']
+
+#Слоты
+Slots = ['Слоты']
+
+#Баскетбол
+Basketball = ['Баскетбол Гол', 'Баскет Гол', 'Баскетбол Мимо', 'Баскет Мимо']
+
+#Футбол
+Fotbool = ['Футбол Гол', 'Фут Гол', 'Футбол Мимо', 'Фут Мимо']
+
+#Камень,Ножницы,Бумага
+SU_E_FA = ['Камень', 'Ножницы', 'Бумага']
+
+
+#Колесо
+WHEEL = ['Красное', 'Черное', 'Зеленое']
+
+
+all_text = ['Больше', 'Меньше', '1', '2', '3', '4', '5', '6', 'Пвп', 'Дуэль', '2М', '2Б', '2 Больше', '2 Меньше', 'Чет', 'Нечет',
+            'Слоты', 'Баскетбол Гол', 'Красное', 'Черное', 'Зеленое', 'Баскет Гол', 'Ничья', 'Равно', 'Баскетбол Мимо', 'Баскет Мимо', 'Футбол Гол', 'Фут Гол', 'Футбол Мимо', 'Фут Мимо', 'Камень', 'Ножницы', 'Бумага']
+
+#Красные/Черные/Зеленое
+RED = ['1', '2', '3', '4', '5', '6', '7']
+
+BLACK = ['8', '9', '10', '11', '12', '13', '14']
+
+GREEN = '0'
+
